79e5337f790ffda37087fbe65efe97f29333e412
Justes <2395781272@qq.com>
2023-07-05 11:59:41 +0100

    mod dataset

--- main.py
+++ main.py
@@ -65,18 +65,14 @@ def main():
     train_dataloader = DataLoader(VideoDataset(dataset=dataset, split='train', clip_len=frame, root=root),
                                   batch_size=batch_size,
                                   shuffle=True, num_workers=args.workers)
-    val_dataloader = DataLoader(VideoDataset(dataset=dataset, split='val', clip_len=frame, root=root),
-                                batch_size=batch_size,
-                                num_workers=args.workers)
     test_dataloader = DataLoader(VideoDataset(dataset=dataset, split='test', clip_len=frame, root=root),
                                  batch_size=batch_size,
                                  num_workers=args.workers)
     print("train len:", len(train_dataloader))
-    print("val   len:", len(val_dataloader))
     print("test  len:", len(test_dataloader))
 
-    trainval_loaders = {'train': train_dataloader, 'val': val_dataloader}
-    trainval_sizes = {x: len(trainval_loaders[x].dataset) for x in ['train', 'val']}
+    trainval_loaders = {'train': train_dataloader}
+    trainval_sizes = {x: len(trainval_loaders[x].dataset) for x in ['train']}
     test_size = len(test_dataloader.dataset)
 
     criterion = nn.CrossEntropyLoss()  # standard crossentropy loss for classification
@@ -116,46 +112,36 @@ def main():
 
     for epoch in range(start_epoch, epochs):
         # each epoch has a training and validation step
-        for phase in ['train', 'val']:
-            start_time = timeit.default_timer()
+        phase = "train"
+        start_time = timeit.default_timer()
 
-            # reset the running loss and corrects
-            running_loss = 0.0
-            running_corrects = 0.0
+        # reset the running loss and corrects
+        running_loss = 0.0
+        running_corrects = 0.0
 
-            # set model to train() or eval() mode depending on whether it is trained
-            # or being validated. Primarily affects layers such as BatchNorm or Dropout.
-            if phase == 'train':
-                # scheduler.step() is to be called once every epoch during training
-                model.train()
-            else:
-                model.eval()
+        # set model to train() or eval() mode depending on whether it is trained
+        # or being validated. Primarily affects layers such as BatchNorm or Dropout.
 
-            for inputs, labels in trainval_loaders[phase]:
-                # move inputs and labels to the device the training is taking place on
-                inputs = Variable(inputs, requires_grad=True).to(device)
-                labels = Variable(labels).to(device)
-                optimizer.zero_grad()
+        # scheduler.step() is to be called once every epoch during training
+        model.train()
 
-                if phase == 'train':
-                    outputs = model(inputs)
-                else:
-                    with torch.no_grad():
-                        outputs = model(inputs)
+        for inputs, labels in trainval_loaders[phase]:
+            # move inputs and labels to the device the training is taking place on
+            inputs = Variable(inputs, requires_grad=True).to(device)
+            labels = Variable(labels).to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
 
-                probs = nn.Softmax(dim=1)(outputs)
-                preds = torch.max(probs, 1)[1]
-                loss = criterion(outputs, labels)
+            probs = nn.Softmax(dim=1)(outputs)
+            preds = torch.max(probs, 1)[1]
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
 
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            if phase == 'train':
-                scheduler.step()
+            scheduler.step()
 
             epoch_loss = running_loss / trainval_sizes[phase]
             epoch_acc = running_corrects.double() / trainval_sizes[phase]
@@ -165,11 +151,6 @@ def main():
                 writer.add_scalar('data/train_acc_epoch', epoch_acc, epoch + 1)
                 train_loss.append(round(epoch_loss, 4))
                 train_acc.append(round(epoch_acc.item(), 4))
-            else:
-                writer.add_scalar('data/val_loss_epoch', epoch_loss, epoch + 1)
-                writer.add_scalar('data/val_acc_epoch', epoch_acc, epoch + 1)
-                val_loss.append(round(epoch_loss, 4))
-                val_acc.append(round(epoch_acc.item(), 4))
 
             lr_tmp = scheduler.get_last_lr()[0]
             print("[{}] Epoch: {}/{} Loss: {:.4f} Acc: {:.4f} Lr: {:.6f}".format(phase, epoch + 1, epochs, epoch_loss, epoch_acc, lr_tmp))
@@ -221,8 +202,8 @@ def main():
     print("==================================")
     print("train acc:", train_acc)
     print("train loss:", train_loss)
-    print("val acc:", val_acc)
-    print("val loss:", val_loss)
+    # print("val acc:", val_acc)
+    # print("val loss:", val_loss)
     print("test acc:", test_acc)
     print("test loss:", test_loss)
 
--- train.sh
+++ train.sh
@@ -3,7 +3,6 @@
 python main.py \
 -a r2p1d_18 \
 -s hmdb51 \
---frame 8 \
 --max-epoch 1 \
 --pretrained-model ../pretrained/r2p1d18_K_200ep.pth \
 --save-dir logs
